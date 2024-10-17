0894940223819595f68ab768013331828702c561
Pedro Romão <prcd20020@gmail.com>
2024-10-17 19:33:45 -0300

    Add update route

--- app.py
+++ app.py
@@ -66,6 +66,31 @@ def delete_task(id):
         db.session.rollback()
         return jsonify({"error": "Erro ao deletar a tarefa", "details": str(e)}), 500
 
+@app.route('/task/update/<int:id>', methods=['PUT'])
+def update_task(id):
+    try:
+        task = Task.query.get(id)
+        
+        if not task:
+            return jsonify({"error": "Tarefa não encontrada"}), 404
+        
+        status = request.json.get('status')
+        
+        if status is None:
+            return jsonify({"error": "O campo 'status' é obrigatório."}), 400
+        
+        if status == task.status:
+            return jsonify({"message": "Status era o mesmo, sem alterações."}), 200
+        
+        task.status = status
+        
+        db.session.commit()
+
+        return jsonify({"message": "Status da tarefa atualizado com sucesso!"})
+    
+    except Exception as e:
+        return jsonify({"error": "Erro ao atualizar a tarefa", "details": str(e)}), 500
+
 
 if __name__ == '__main__':
     with app.app_context():
