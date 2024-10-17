8596d60c4ce80b99106ef402d5f1073d662536a3
Pedro Romão <prcd20020@gmail.com>
2024-10-17 18:19:04 -0300

    Create postgres database

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask
+from models import db
+from models.task import Task
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost/todo_db'
+
+db.init_app(app)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
